44f37925c75594bc951b905bc3faaafc61a7d2f5
Seth Terry <isethguy@gmail.com>
2021-03-03 19:33:51 -0500

    dailey math update

--- moduleNameUpdate/index.js
+++ moduleNameUpdate/index.js
@@ -1,44 +1,58 @@
-const firebase = require('../firebase');
-const db = firebase.firestore();
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../epicc-admin-firebase-adminsdk-o0h1d-54e583caeb.json");
+const shoppdfbucket = "epicc-admin.appspot.com";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: shoppdfbucket
+});
+
+const db = admin.firestore();
 const questionsRef = db.collection('sheetQuestions');
-const areasRef = db.collection('sheetAreas');
-const modsRef = db.collection('sheetMods');
-var PDFImage = require("pdf-image").PDFImage;
-var path = require('path')
-var os = require('os')
-var axios = require('axios')
-var fs = require('fs')
 
 // const shoppdfbucket = "epicc-admin.appspot.com";
 // const storageRef = firebase.storage()
 
-const signIn = async () => {
-  try {
-    const response = await firebase.auth().signInWithEmailAndPassword('isethguy@gmail.com', 'sethwins')
-  } catch (error) {
-    console.log("signIn -> error", error)
-  }
-}
-
 const update = async () => {
 
-  await signIn()
-
-  const result = await questionsRef.where('moduleName', '==', 'Object Description 1').get()
+  const result = await questionsRef.where('moduleName', '==', 'Daily Math').get()
   //  console.log("update -> result", result.docs.length)
 
-  result.docs.forEach(async (doc) => {
-    //console.log("update -> doc", doc)
+  const oneShots = result.docs.filter(doc => {
 
     const data = doc.data();
     console.log("update -> data", data)
+    const { questionSet = [] } = data;
+    return questionSet.length == 0
+  })
 
+  oneShots.forEach(async (doc) => {
+    //console.log("update -> doc", doc)
 
+    const data = doc.data();
+    // console.log("update -> data", data)
+
+    const { challangePicture, wrongAnswerText, challangeTime, challangeText, answerText } = data
     // console.log("update -> doc", doc.data())
 
+    const questionSet = [{
+      challangePicture,
+      wrongAnswerText,
+      challangeTime,
+      challangeText,
+      answerText,
+    }]
+    const itemUpdate = {
+      questionSet
+    }
 
-    questionsRef.doc(doc.id).update({ moduleName: 'Object Description' })
+    // await questionsRef.doc(doc.id).update({
+    //   ...itemUpdate,
+    // });
 
+    console.log("update -> itemUpdate", questionSet)
 
   })
 
